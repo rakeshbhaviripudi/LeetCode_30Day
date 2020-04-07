7bb0cca58e8b0cdf45c517a4e43921a1822d03e3
rakeshbhaviripudi <61092568+rakeshbhaviripudi@users.noreply.github.com>
2020-04-07 14:08:35 +0530

    Create Group Anagrams.cpp

---
+++ Week 1/Group Anagrams.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>>mp;
+        vector<vector<string>> result;
+        int i=0;
+        for(string s : strs){
+            sort(s.begin(), s.end());
+            mp[s].push_back(strs[i++]);
+        }
+        for(auto p : mp){
+            sort(p.second.begin(), p.second.end());
+            result.push_back(p.second);
+        }
+        
+        return result;
+    }
+};
+auto speedup=[](){
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
