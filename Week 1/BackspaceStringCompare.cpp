abd2b7fa11e178adf7cb446665a176b7b087e144
rakeshbhaviripudi <61092568+rakeshbhaviripudi@users.noreply.github.com>
2020-04-09 18:47:38 +0530

    Create BackspaceStringCompare.cpp

---
+++ Week 1/BackspaceStringCompare.cpp
@@ -0,0 +1,47 @@
+class Solution {
+    /* THIS SOLUTION IS NOT OPTIMAL, THIS GIVES O(N) TIME COMPLEXITY BUT NOT O(1)
+    *  SPACE COMPLEXITY. IT WILL TAKE APPROX. O(N) SPACE COMPLEXITY.
+    *
+    */
+public:
+    bool backspaceCompare(string S, string T) {
+        stack<char> s;
+        stack<char> t;
+        for(int i=0; i<S.length();i++){
+            if(S[i] != '#') s.push(S[i]);
+            if(S[i] == '#'){
+                if(s.empty()) continue;
+                else{
+                    s.pop();
+                }
+            }
+        }
+         for(int i=0; i<T.length();i++){
+            if(T[i] != '#') t.push(T[i]);
+            if(T[i] == '#') {
+                if(t.empty())continue;
+                else{
+                    t.pop();
+                }
+            }
+        }
+        if(s.size() != t.size()) return false;
+        
+        bool flag = true;
+
+        while(s.empty() == false){
+             cout<<s.top()<<" "<<t.top();
+            if(s.top() == t. top()){
+                s.pop();
+                t.pop();
+            }
+            else { 
+                 cout<<s.top()<<" "<<t.top();
+                flag = false; 
+                break; 
+            } 
+        }
+        
+    return flag;
+    }
+};
