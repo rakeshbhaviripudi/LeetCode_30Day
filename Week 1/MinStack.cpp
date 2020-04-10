ea23927a40120dc496b3d290ca38384e0baad17c
rakeshbhaviripudi <61092568+rakeshbhaviripudi@users.noreply.github.com>
2020-04-10 18:47:31 +0530

    Create MinStack.cpp

---
+++ Week 1/MinStack.cpp
@@ -0,0 +1,38 @@
+
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    stack<pair<int,int>>s;
+    int minVal;
+    MinStack() {
+        minVal = INT_MAX;
+    }
+    
+    void push(int x) {     
+        if(x< minVal)
+            minVal = x;
+        s.push({x,minVal});
+    }
+    
+    void pop() {
+        s.pop();
+        minVal = s.empty()?INT_MAX:s.top().second;
+    }
+    
+    int top() {
+       return s.top().first;
+    }
+    
+    int getMin() {
+      return  s.top().second;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
