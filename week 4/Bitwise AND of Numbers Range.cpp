4ee61819ec7a368431b63281c524fbd7e37306c1
Rakesh Bhaviripudi <61092568+rakeshbhaviripudi@users.noreply.github.com>
2020-04-23 21:30:18 +0530

    Update Bitwise AND of Numbers Range.cpp

--- week 4/Bitwise AND of Numbers Range.cpp
+++ week 4/Bitwise AND of Numbers Range.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int rangeBitwiseAnd_naive(int m, int n) {
         if(m==0) return 0;
-        if(n-m > 10000000) return 0;
+        if(n-m > 10000000) return 0; //BruteForce Hard Coded.
         int i;
         //int ans=1;
         for(i=n-1; i >=m; i--){
